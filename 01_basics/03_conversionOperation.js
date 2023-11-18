48dca059af2769eb876635934a5ea3f5769f9c25
Bhavya H Vyas <136875582+BhavyaV-1124@users.noreply.github.com>
2023-11-18 06:28:21 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33abc";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/* 
+ When number '33' => 33
+ '33abc' => Nan
+ true => 1; false => 0
+*/
+
+let isLoggedIn = "Bhavya";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
