3f7c8bb435c0d4b1d7acfbc2c3591267b7cbf71f
Bhavya H Vyas <136875582+BhavyaV-1124@users.noreply.github.com>
2023-11-18 06:02:31 +0000

    variable and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "Bhavya@google.com"
+var accountPassword = "12345"
+acocuntCity = "Abad"
+
+//  accountId = 2
+accountEmail ="jay@google.com"
+accountPassword = "212221"
+accountCity = "Bengluru"
+
+/*
+Prefer not to use var..
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use Strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodeJs, not browser need diffrent method for node
+
+let numbert = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+/*
+Types of dataTypes
+1: number => 2^53
+2: bigint
+3: String => ""
+4: boolean => true/false
+5: null => standalone value / it's an object
+6: undefined => 
+7: symbol => unique
+
+8: object => 
+
+to find given value's dataType always opt with 'console.log(typeof {dataType_value});'
+*/
+
+console.log(typeof age);
