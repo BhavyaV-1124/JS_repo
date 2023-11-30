397ce6bad5e83ec6af156be5d47462822abe7910
Bhavya H Vyas <136875582+BhavyaV-1124@users.noreply.github.com>
2023-11-30 10:28:09 +0000

    data type summary done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+
+/* 
+
+Primitive DataTypes: (call by value)
+    7 Types:   String, Number, Boolean, null, undefined, Symbol, BigInt
+
+Refrence Type(Non Primitive)
+    Types: Array, Objects, Functions.
+
+JavaScript is Dynamically typed language unlike Java which is Statically typed..
+
+*/
+
+const outSideTemp = null; // null
+let userEmail; // undefined
+const bigNumber = 345566433445644n; // give n to make it BigInt
+const id = symbol('123'); // symbol makes 2 identical values different
+
+const heroes = ["Shaktiman", "Nagraj", "doga"]; //Array
+
+const obj = {
+    person: "Bhavya",  // Object
+    age: 25
+}
+
+const myFunction = function(){
+    console.log("Hello World"); // Function
+}
+
