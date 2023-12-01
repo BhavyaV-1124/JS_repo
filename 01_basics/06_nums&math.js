483667a57082222857d42e598d6708a2647c1efe
Bhavya H Vyas <136875582+BhavyaV-1124@users.noreply.github.com>
2023-12-01 10:06:23 +0000

    nums&math done.

---
+++ 01_basics/06_nums&math.js
@@ -0,0 +1,31 @@
+
+// ***************** nums **************
+
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+
+const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toFixed(4));
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// *********** Maths **************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.sqrt(25));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+ min);
