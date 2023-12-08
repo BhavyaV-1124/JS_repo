792b670c8e133eae678aae4f3b36678b99842873
Bhavya H Vyas <136875582+BhavyaV-1124@users.noreply.github.com>
2023-12-08 12:06:02 +0000

    scopes done.

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,12 +3,50 @@ const b = 20;
 var c = 30;
 
 
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// Function scope
+
+function one(){
+    const username = "Bhavya"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+
+// one();
+
+// 
+
+if(true){
+    const username = "Bhavya";
+    if(username === "Bhavya"){
+        const website =" youtube";
+        // console.log(username + website);
+    }
+
+}
 
 
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& Intresting &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+function addOne(num){
+    return num +1;
+}                                       // taken as function
+
+console.log(addOne(5));
 
 
+const addTwo = function(num){
+    return num + 2;
+}
 
+console.log(addTwo(4));                 // talken as expression
 
-console.log(a);
-console.log(b);
-console.log(c);
+// So the point is if you hoist function as a expression before the function it will give error.
